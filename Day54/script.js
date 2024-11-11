578a55c9140e89b34e6d65c138afd66b04d204ca
C2W-flutter-Projects <flutteravishkar@gmail.com>
2024-11-11 17:06:50 +0530

    Javascript Introduction

---
+++ Day54/script.js
@@ -0,0 +1,15 @@
+alert("HEllo World")
+console.log('code is running')
+console.log("code is looking like a wow...")
+
+var a = prompt("Enter Your Number")
+
+var isTrue = confirm("Are you Sure You are the Don")
+
+if(isTrue){
+    console.log("Yes... You Are The Don")
+}else {
+    console.log("My Bad")
+}
+
+console.log("Your number is "+ a);
