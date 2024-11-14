e75762c5b71e298d86a7a6f2156a429df9bcde28
C2W-flutter-Projects <flutteravishkar@gmail.com>
2024-11-14 20:09:01 +0530

    Exercise 9

---
+++ Day59/index.js
@@ -0,0 +1,16 @@
+/*
+
+Create a faulty Calculator
+
+1: It takes two numbers as input from user
+2: It should perform the operations as follows:
+
++ ---> -
+* ---> +
+- ---> /
+/ ---> **
+
+
+It performs wrong operation 10% of the times
+
+*/
