14895ad18825c0bb6d87f9d4b649708843c37368
C2W-flutter-Projects <flutteravishkar@gmail.com>
2024-11-14 21:00:39 +0530

    Strings in Javascript

---
+++ Day60/index.js
@@ -0,0 +1,25 @@
+console.log("This is Tutorial of Javascript Strings");
+
+let a = "Avi"
+
+console.log(a[0]);
+console.log(a[1]);
+console.log(a[2]);
+console.log(a[3]);
+
+let myname = "Avi"
+let suffix = "DADA"
+let friend = "Sandy"
+
+console.log("My name is " +myname+ " and my friend name is " + friend);
+console.log(`My name is ${myname}  and my friend name is ${friend}`);
+
+let b = "AVishkar"
+
+console.log(b.toLowerCase())
+console.log(b.toUpperCase())
+console.log(b.length)
+console.log(b.slice(1,6));
+console.log(b.slice(1));
+console.log(b.replace("AV" ,"AAAV"))
+console.log(b.concat(myname,suffix))
