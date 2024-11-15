55f4efb7686b92cec8b0fdc9ec41746b749d4b20
C2W-flutter-Projects <flutteravishkar@gmail.com>
2024-11-15 10:56:19 +0530

    Excercise-10- Business Name Generator

---
+++ Day62/index.js
@@ -0,0 +1,21 @@
+/*
+
+create a Business Name Generator without using the arrays which will create the business names from the given set of words and adjectives
+
+Adjectives:
+Crazy
+Amazing
+Fire
+
+Shop Name:
+Engine
+Food
+Garments
+
+Another name:
+Bros
+Limited
+Hub
+
+
+*/
