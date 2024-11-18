34c511c6f076073722dd0ebfed6f66268c1517ba
C2W-flutter-Projects <flutteravishkar@gmail.com>
2024-11-18 16:51:27 +0530

    Inserting and Deleting element from HTML

---
+++ Day71/script.js
@@ -0,0 +1,18 @@
+// document.querySelector(".container").innerHTML
+// document.querySelector(".container").innerText
+// document.querySelector(".box").innerHTML
+// document.querySelector(".box").innerText
+// document.querySelector(".container").tagName
+// document.querySelector(".container").textContent
+// .getAttribute
+// .setAttribute
+// .removeAttribute
+// .hasAttribute
+// .Attributes
+// .dataset
+//beforbegin
+//afterbegin
+//beforeend
+//afterend
+//classList.toggle
+
